3f0de7817c14329224254568e0beed4cdcd7cef6
Danny Teixeira <dteixeira@itadmins-mbp-3.relias.int>
2016-10-03 17:12:29 -0400

    Routing Chapter up to - Move content out of the component file

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,36 @@
+// 3rd Party
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+
+
+// Internal Components
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { routing } from './app.routing';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
+
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
+
+  providers: [
+    HeroService
+  ],
+
   bootstrap: [ AppComponent ]
 })
   
-export class AppModule { }
+export class AppModule {}
